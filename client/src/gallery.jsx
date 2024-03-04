ccd12b0870459e81460fab7ef943feacdee31e6c
Manoj0012 <anoj9626@gmail.com>
2024-03-04 14:54:48 +0530

    --
    --

---
+++ client/src/gallery.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Logo from './logo'
+function gallery() {
+  return (
+    <div className='w-[100vh] h-[100vh] bg-black'>
+      <div className='w-[100vh]'><Logo/></div>
+    </div>
+  )
+}
+
+export default gallery
